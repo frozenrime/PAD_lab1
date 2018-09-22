338402b44135f4a90c5567939f4d481cf1c22bf4
Cristian Godonoaga <cris.go92@gmail.com>
2018-09-22 13:19:22 +0300

    Serialization of send data are fixed

--- PublisherSubcriberApp/BasicAsyncClient/ClassPublisher.cs
+++ PublisherSubcriberApp/BasicAsyncClient/ClassPublisher.cs
@@ -7,36 +7,34 @@ namespace Publisher
     class ClassPublisher
     {
         public string Host { get; set; }
-        public string Ip { get; set; }
+        public string Canal_Id { get; set; }
         public string Message { get; set; }
 
-        public ClassPublisher(string host, string ip, string message)
+        public ClassPublisher(string host, string id, string message)
         {
             Host = host;
-            Ip = ip;
+            Canal_Id = id;
             Message = message;
         }
 
-        /// Deserializarea datelor primite
-        public ClassPublisher(byte[] data)
-        {
-            int Hostlengh = BitConverter.ToInt32(data, 1);
-            Host = Encoding.ASCII.GetString(data, 7, Hostlengh);
-            int IpLengh = BitConverter.ToInt32(data, 1);
-            Ip = Encoding.ASCII.GetString(data, 7, Hostlengh);
-            int messageLength = BitConverter.ToInt32(data, 3);
-            Message = Encoding.ASCII.GetString(data, 7, messageLength);
-        }
+        ///// Deserializarea datelor primite
+        //public ClassPublisher(byte[] data)
+        //{
+        //    int Hostlengh = BitConverter.ToInt32(data, 1);
+        //    Host = Encoding.ASCII.GetString(data, 7, Hostlengh);
+        //    int IpLengh = BitConverter.ToInt32(data, 1);
+        //    Ip = Encoding.ASCII.GetString(data, 7, Hostlengh);
+        //    int messageLength = BitConverter.ToInt32(data, 3);
+        //    Message = Encoding.ASCII.GetString(data, 7, messageLength);
+        //}
 
         ///  Serializarea pachetului de date intr-un array
         public byte[] ToByteArray()
         {
             List<byte> byteList = new List<byte>();
-            byteList.AddRange(BitConverter.GetBytes(Host.Length));
-            byteList.AddRange(Encoding.ASCII.GetBytes(Host));
-            byteList.AddRange(BitConverter.GetBytes(Ip.Length));
-            byteList.AddRange(Encoding.ASCII.GetBytes(Ip));
-            byteList.AddRange(BitConverter.GetBytes(Message.Length));
+            int id = Int32.Parse(Canal_Id);
+
+            byteList.AddRange(BitConverter.GetBytes(id));
             byteList.AddRange(Encoding.ASCII.GetBytes(Message));
             return byteList.ToArray();
         }
